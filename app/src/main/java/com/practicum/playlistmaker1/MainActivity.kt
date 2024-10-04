2b98c65e28943ec7829c882c1d05ff82d2a107f5
Hakkuda <grischa234@mail.ru>
2024-10-04 14:50:16 +0200

    Исправление 1

--- app/src/main/java/com/practicum/playlistmaker1/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker1/MainActivity.kt
@@ -3,14 +3,12 @@ package com.practicum.playlistmaker1
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
         setContentView(R.layout.activity_main)
         val searchButton = findViewById<Button>(R.id.search)
--- app/src/main/java/com/practicum/playlistmaker1/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker1/SettingsActivity.kt
@@ -1,22 +1,19 @@
 package com.practicum.playlistmaker1
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
 
         setContentView(R.layout.activity_settings)
         val backToMainButton = findViewById<View>(R.id.back_to_main_button)
 
         backToMainButton.setOnClickListener {
-            val displayIntent = Intent(this, MainActivity::class.java)
-            startActivity(displayIntent)
+            this.finish()
         }
 
     }
